d10d4df807a092d042ed9b8f29c5eac12011e21a
shaansen <shantanu.sengupta2008@gmail.com>
2020-08-01 20:06:49 -0700

    Add immediate result file

---
+++ src/ImmediateResult.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { AnswerObject } from "./API";
+
+const ImmediateResult = (props: { userAnswer: AnswerObject | undefined }) => {
+  const { userAnswer } = props;
+  let text = null;
+  if (!!userAnswer) {
+    const { correct, correctAnswer, answer } = userAnswer;
+    if (correct) {
+      text = "CORRECT";
+    } else {
+      text = "INCORRECT";
+    }
+  }
+  return (
+    <div className="immediate-result">
+      <h1>{text}</h1>
+    </div>
+  );
+};
+
+export default ImmediateResult;
